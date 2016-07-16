b316a1341b51e9156a7f37dcde47823b0e08cbc2
mrexe1 <www.gmbteam@gmail.com>
2016-07-16 15:55:53 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {125462953,48232451,226735576},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[MeGa shield v2.9
 An advanced administration bot based on TG-CLI written in Lua
